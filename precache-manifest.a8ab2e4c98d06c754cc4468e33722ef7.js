6d58ce9f56912dc50a0a875dcc68ff07c048645d
Tian Chen <me@tian.im>
2020-07-29 23:17:21 +1000

    Updates

---
+++ precache-manifest.a8ab2e4c98d06c754cc4468e33722ef7.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9fa0e1f49c8064bd24f95adb0d9e3f39",
+    "url": "/index.html"
+  },
+  {
+    "revision": "142e61730ee368fec22c",
+    "url": "/static/css/main.b7af2f91.chunk.css"
+  },
+  {
+    "revision": "a60d415e9654416de1de",
+    "url": "/static/js/2.7e765fb8.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7e765fb8.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142e61730ee368fec22c",
+    "url": "/static/js/main.1f92f757.chunk.js"
+  },
+  {
+    "revision": "7dba71482664d4876470",
+    "url": "/static/js/runtime-main.9d1bbad9.js"
+  }
+]);
